f22af0274d6da1a6e3033c5349037cb875d6f9d3
gilbert-ku <gilbert45ku@gmail.com>
2023-05-24 22:38:51 +0300

    add variable to JS

---
+++ tile.js
@@ -0,0 +1,8 @@
+const selectors = {
+    boardContainer: document.querySelector('.board-container'),
+    board: document.querySelector('.board'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('button'),
+    win: document.querySelector('.win')
+}
