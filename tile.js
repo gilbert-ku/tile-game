2917cbc38a7e42ab48ab090341beb5fd7091900a
gilbert-ku <gilbert45ku@gmail.com>
2023-05-24 22:40:19 +0300

    add variable to game state

--- tile.js
+++ tile.js
@@ -6,3 +6,12 @@ const selectors = {
     start: document.querySelector('button'),
     win: document.querySelector('.win')
 }
+
+//game state
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
