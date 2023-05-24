81ce6bda1fea804671ecdd409583aaeb41479621
gilbert-ku <gilbert45ku@gmail.com>
2023-05-24 22:45:17 +0300

    add mate oparator - random

--- tile.js
+++ tile.js
@@ -29,3 +29,18 @@ const shuffle = array => {
 
     return clonedArray
 }
+
+//math-random
+const pickRandom = (array, items) => {
+    const clonedArray = [...array]
+    const randomPicks = []
+
+    for (let index = 0; index < items; index++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length)
+
+        randomPicks.push(clonedArray[randomIndex])
+        clonedArray.splice(randomIndex, 1)
+    }
+
+    return randomPicks
+}
