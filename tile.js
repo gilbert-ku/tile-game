9a75bcb61436cfb13d44e361bb48c1fe9109eae1
gilbert-ku <gilbert45ku@gmail.com>
2023-05-24 22:51:05 +0300

    add function to start and stop

--- tile.js
+++ tile.js
@@ -71,3 +71,24 @@ const generateGame = () => {
 
     selectors.board.replaceWith(parser.querySelector('.board'))
 }
+
+//star&stp
+const startGame = () => {
+    state.gameStarted = true
+    selectors.start.classList.add('disabled')
+
+    state.loop = setInterval(() => {
+        state.totalTime++
+
+        selectors.moves.innerText = `${state.totalFlips} moves`
+        selectors.timer.innerText = `time: ${state.totalTime} sec`
+    }, 1000)
+}
+
+const flipBackCards = () => {
+    document.querySelectorAll('.card:not(.matched)').forEach(card => {
+        card.classList.remove('flipped')
+    })
+
+    state.flippedCards = 0
+}
