8c42ab3ad77b2e8f7a50c7c0f2f31432d700bd21
gilbert-ku <gilbert45ku@gmail.com>
2023-05-24 22:43:20 +0300

    add function to shuffle

--- tile.js
+++ tile.js
@@ -15,3 +15,17 @@ const state = {
     totalTime: 0,
     loop: null
 }
+//function
+const shuffle = array => {
+    const clonedArray = [...array]
+
+    for (let index = clonedArray.length - 1; index > 0; index--) {
+        const randomIndex = Math.floor(Math.random() * (index + 1))
+        const original = clonedArray[index]
+
+        clonedArray[index] = clonedArray[randomIndex]
+        clonedArray[randomIndex] = original
+    }
+
+    return clonedArray
+}
